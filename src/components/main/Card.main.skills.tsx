5ccbfe8b0edb69259aba43b63a94ca4a971950ca
Côme Chevallier <come.chevallier@icloud.com>
2023-10-23 18:26:03 +0200

    space correction on card main skills components

--- src/components/main/Card.main.skills.tsx
+++ src/components/main/Card.main.skills.tsx
@@ -12,16 +12,7 @@ import CardMainSkillsItem from "./Card.main.skills.item";
 
 export default function CardMainSkills() {
   return (
-    <div className="flex items-center justify-between mt-10 px-10 w-full max-w-[2000px] mx-auto  overflow-x-scroll py-4 space-x-2">
-      {/* <CardMainSkillsItem Icon={FaHtml5} text="HTML5" color="orange" />
-      <CardMainSkillsItem Icon={FaJsSquare} text="Javascript" color="yellow" />
-      <CardMainSkillsItem Icon={FaNodeJs} text="NodeJs" color="emerald" />
-      <CardMainSkillsItem Icon={TbBrandNextjs} text="NextJS" color="black" />
-      <CardMainSkillsItem
-        Icon={BiLogoTailwindCss}
-        text="tailwindcss"
-        color="sky"
-      /> */}
+    <div className="flex items-center justify-between mt-10 px-10 w-full max-w-[800px] mx-auto  overflow-x-scroll py-4 space-x-2">
       <CardMainSkillsItem
         Icon={FaCode}
         text="frontend"
